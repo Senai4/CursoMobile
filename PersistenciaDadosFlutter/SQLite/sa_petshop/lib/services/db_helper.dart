b445ab1846a6395cc7e6448c9c02fd857ac64d29
senaisp5@gmail.com <senaisp5@gmail.com>
2025-06-04 08:04:21 -0300

    03.06.2025

--- PersistenciaDadosFlutter/SQLite/sa_petshop/lib/services/db_helper.dart
+++ PersistenciaDadosFlutter/SQLite/sa_petshop/lib/services/db_helper.dart
@@ -1,37 +1,102 @@
+//clase de apoio a conexões do banco de dados
+//classe Singleton -> de objeto unico
+
 import 'package:path/path.dart';
+import 'package:sa_petshop/models/pet_model.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite/sqlite_api.dart';
 
+import '../models/pet_model.dart';
+
 class DbHelper {
-  static Database? _database; //obj para criar as conexões
-
-  //transformar esse classe em singleton
-  //não permite instanciar outro obj enquento um obj estiver ativo
+  static Database? _database; //obj para criar as conexões com o BD
+  //transformar a classe em Singleton
+  // não permite instanciar outro objeto enquanto um obj estiver ativo
   static final DbHelper _instance = DbHelper._internal();
-
-  //Construir o Singleton
+  //construtor para o singleton
   DbHelper._internal();
-  factory DbHelper(){
-    return _instance;
-  }
+  factory DbHelper() => _instance;
 
-  //conexões do Banco de Dados
-  Future<Database> get database async{
-    if(_database != null){
-      return _database!; //se o banco já estiver funcionando , retorna ele mesmo
-    } else{
-      _database = await _initDatabase();
+  //fazer as conexões com o BD
+  Future<Database> get database  async{
+    if (_database != null){
+      return _database!;
+    }else{
+      _database =  await _initDatabase();
       return _database!;
     }
   }
 
   Future<Database> _initDatabase() async{
-    // pegar o local onde esta salvo o BD (path)
-    final _dbPath = await getDatabasesPath();
-    final path = join(_dbPath,"petshop.db"); // caminho para o banco de dados 
+    //pegar o endereco do Banco de Dados
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath,"petshop.db");//caminho completo para o BD
 
-   return await openDatabase(path); // cenas para o próximo capítulo
-
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreateDB
+    );
   }
 
+  Future<void> _onCreateDB(Database db, int version) async{
+    //Criar a Tabel dos pets
+    await db.execute(
+      """CREATE TABLE IF NOT EXISTS pets(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nome TEXT NOT NULL,
+      raca TEXT NOT NULL,
+      nome_dono TEXT NOT NULL,
+      telefone TEXT NOT NULL
+      )"""
+    );
+    print("tabela pet criada");
+    //criar a tabela das Consultas
+    await db.execute(
+      """CREATE TABLE IF NOT EXISTS consultas(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      pet_id INTEGER NOT NULL,
+      data_hora TEXT NOT NULL,
+      tipo_servico TEXT NOT NULL,
+      observacao TEXT,
+      FOREIGN KEY (pet_id) REFERENCES pets(id) ON DELETE CASCADE
+      )"""
+    );
+    print("tabela consultas criada");
+  }
+
+  // métodos CRUD para pets
+  Future<int> insertPet(Pet pet) async{
+    final db = await database;
+    return await db.insert("pets", pet.toMap()); //retorna o id do Pet
+  }
+
+  Future<List<Pet>> getPets() async{
+    final db = await database;
+    final List<Map<String,dynamic>> maps = await db.query("pets"); //receber as info do BD
+    //converter os valores para obj
+    return maps.map((e)=>Pet.fromMap(e)).toList();
+  }
+
+  Future<Pet?> getPetbyId(int id) async{
+    final db = await database;
+    final List<Map<String,dynamic>> maps = 
+      await db.query("pets", where: "id=?", whereArgs: [id]);
+    //se Encontrado
+    if(maps.isNotEmpty){
+      return Pet.fromMap(maps.first); //cria o obj com 1º elemento da lista
+    }else{
+      return null;
+    }
+  }
+
+  Future<int> deletePet(int id) async{
+    final db = await database;
+    return await db.delete("pets", where: "id=?",whereArgs: [id]);
+    //deleta o pet da tabela que tenha o id igual o enviado como parâmetro
+  }
+
+  // métodos crud para Consultas
+
+ 
 }
